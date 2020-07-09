dc2dada1ed9007b85e233241c312f81afc9333bf
heemharr <taiwoo431@gmail.com>
2020-07-09 15:09:30 +0100

    Implemented async await

--- country.js
+++ country.js
@@ -3,30 +3,29 @@ const search = document.getElementById('search');
 const fullInfo = document.getElementById("full-info");
 const DISPLAY_DETAILS = () => {
   //Array.from(article.children).map(item => {
-    //item.addEventListener("click", function(){
-     // console.log("yes")
-   // }());
-    //console.log(item)})
-    
-//  fetch("https://restcountries.eu/rest/v2/all").then(resp => resp.json())
+  //item.addEventListener("click", function(){
+  // console.log("yes")
+  // }());
+  //console.log(item)})
+
+  //  fetch("https://restcountries.eu/rest/v2/all").then(resp => resp.json())
   //.then(data => {
-    //let full = "";
-    //data.map(datas => {
-      //full += `<ul>
-        //<li>Region:${datas.region}</li>
-      //</ul>
-     // `
-    //})
-    //article.innerHTML = full;
- // })
- //.catch(err => console.log(err))
+  //let full = "";
+  //data.map(datas => {
+  //full += `<ul>
+  //<li>Region:${datas.region}</li>
+  //</ul>
+  // `
+  //})
+  //article.innerHTML = full;
+  // })
+  //.catch(err => console.log(err))
 }
 
 
-function getCountry(){
-  fetch('https://restcountries.eu/rest/v2/all')
-.then(response => response.json())
-.then(data => {
+async function getCountry() {
+  const resp = await fetch('https://restcountries.eu/rest/v2/all');
+  const data = await resp.json();
   let COUNTRY_INFO = [];
   data.forEach(datas => {
     COUNTRY_INFO.push(`
@@ -41,18 +40,14 @@ function getCountry(){
     </div>
     `);
   });
-article.innerHTML = COUNTRY_INFO.join('');
-  })
-  .catch(err => console.log("error:", err))
-  
+  article.innerHTML = COUNTRY_INFO.join('');
 }
 
-function Filter_Countries(event){
+function Filter_Countries(event) {
   let searchInput = event.target.value.toLowerCase();
 }
 
-search.addEventListener("keyup", Filter_Countries);
-window.onload = function(){
+//search.addEventListener("keyup", Filter_Countries);
+window.onload = function() {
   getCountry();
 }
-
